48f00a6023474c0606f0890724ed821b06e951e5
Nohyun Nehemiah Kwak <nehemiah.kwak@krustuniverse.com>
2023-04-24 19:58:27 +0900

    Add typedTx to factory

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,18 +1,46 @@
 export { TypedTx, TypedTxFactory } from "./tx";
 export { TypedAccountKey, TypedAccountKeyFactory } from "./account";
 
+
+import { TypedTxFactory } from "./tx";
+
+// Basic TX 
 import {
   TypedTxValueTransfer,
   TypedTxValueTransferMemo,
-  TypedTxAccountUpdate,
   TypedTxSmartContractDeploy,
-} from "./klaytn_tx";
+  TypedTxSmartContractExecution,
+  TypedTxAccountUpdate,
+  TypedTxCancel, 
+  TypedTxChainDataAnchoring,
+} from "./klaytn_tx_basic";
 
-import { TypedTxFactory } from "./tx";
 TypedTxFactory.add(TypedTxValueTransfer);
 TypedTxFactory.add(TypedTxValueTransferMemo);
 TypedTxFactory.add(TypedTxSmartContractDeploy);
+TypedTxFactory.add(TypedTxSmartContractExecution);
 TypedTxFactory.add(TypedTxAccountUpdate);
+TypedTxFactory.add(TypedTxCancel);
+TypedTxFactory.add(TypedTxChainDataAnchoring);
+
+// Fee Delegation TX
+import {
+  TypedTxFeeDelegatedValueTransfer,
+  TypedTxFeeDelegatedValueTransferMemo,
+  TypedTxFeeDelegatedSmartContractDeploy,
+  TypedTxFeeDelegatedSmartContractExecution,
+  TypedTxFeeDelegatedAccountUpdate,
+  TypedTxFeeDelegatedCancel, 
+  TypedTxFeeDelegatedChainDataAnchoring,
+} from "./klaytn_tx_fee_delegation";
+
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransfer);
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransferMemo);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractDeploy);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractExecution);
+TypedTxFactory.add(TypedTxFeeDelegatedAccountUpdate);
+TypedTxFactory.add(TypedTxFeeDelegatedCancel);
+TypedTxFactory.add(TypedTxFeeDelegatedChainDataAnchoring);
 
 import {
   TypedAccountKeyLegacy,
