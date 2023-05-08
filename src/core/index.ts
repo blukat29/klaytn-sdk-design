7165d95fdca59d5750d2f6d52d8a2dcbbe89cb0f
Nohyun Nehemiah Kwak <nehemiah.kwak@krustuniverse.com>
2023-05-08 15:16:41 +0900

    Add Klaytn TxTypes

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,65 @@
 export { TypedTx, TypedTxFactory } from "./tx";
 export { TypedAccountKey, TypedAccountKeyFactory } from "./account";
 
-import {
-  TypedTxValueTransfer,
-  TypedTxAccountUpdate,
-} from "./klaytn_tx";
 
 import { TypedTxFactory } from "./tx";
+
+// Basic TX 
+import {
+  TypedTxValueTransfer,
+  TypedTxValueTransferMemo,
+  TypedTxSmartContractDeploy,
+  TypedTxSmartContractExecution,
+  TypedTxAccountUpdate,
+  TypedTxCancel, 
+  TypedTxChainDataAnchoring,
+} from "./klaytn_tx_basic";
+
 TypedTxFactory.add(TypedTxValueTransfer);
+TypedTxFactory.add(TypedTxValueTransferMemo);
+TypedTxFactory.add(TypedTxSmartContractDeploy);
+TypedTxFactory.add(TypedTxSmartContractExecution);
 TypedTxFactory.add(TypedTxAccountUpdate);
+TypedTxFactory.add(TypedTxCancel);
+TypedTxFactory.add(TypedTxChainDataAnchoring);
+
+// Fee Delegation TX
+import {
+  TypedTxFeeDelegatedValueTransfer,
+  TypedTxFeeDelegatedValueTransferMemo,
+  TypedTxFeeDelegatedSmartContractDeploy,
+  TypedTxFeeDelegatedSmartContractExecution,
+  TypedTxFeeDelegatedAccountUpdate,
+  TypedTxFeeDelegatedCancel, 
+  TypedTxFeeDelegatedChainDataAnchoring,
+} from "./klaytn_tx_fee_delegation";
+
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransfer);
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransferMemo);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractDeploy);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractExecution);
+TypedTxFactory.add(TypedTxFeeDelegatedAccountUpdate);
+TypedTxFactory.add(TypedTxFeeDelegatedCancel);
+TypedTxFactory.add(TypedTxFeeDelegatedChainDataAnchoring);
+
+// Partial Fee Delegation TX
+import {
+  TypedTxFeeDelegatedValueTransferWithRatio,
+  TypedTxFeeDelegatedValueTransferMemoWithRatio,
+  TypedTxFeeDelegatedSmartContractDeployWithRatio,
+  TypedTxFeeDelegatedSmartContractExecutionWithRatio,
+  TypedTxFeeDelegatedAccountUpdateWithRatio,
+  TypedTxFeeDelegatedCancelWithRatio, 
+  TypedTxFeeDelegatedChainDataAnchoringWithRatio,
+} from "./klaytn_tx_partial_fee_delegation";
+
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransferWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedValueTransferMemoWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractDeployWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedSmartContractExecutionWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedAccountUpdateWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedCancelWithRatio);
+TypedTxFactory.add(TypedTxFeeDelegatedChainDataAnchoringWithRatio);
 
 import {
   TypedAccountKeyLegacy,
