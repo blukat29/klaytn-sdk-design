030b6b48bec460545f5d75184d83a42d40d4bb60
Nohyun Nehemiah Kwak <nehemiah.kwak@krustuniverse.com>
2023-05-08 14:53:25 +0900

    Create basic_TxTypeValueTransfer.js

---
+++ example/basic_TxTypeValueTransfer.js
@@ -0,0 +1,47 @@
+const ethers = require("ethers");
+const { KlaytnWallet } = require("../dist/src/ethers"); // require("@klaytn/sdk-ethers");
+
+const fs = require('fs')
+const privateKey1 = fs.readFileSync('./example/privateKey', 'utf8') // private key of sender 
+
+const account1 = '0x3208ca99480f82bfe240ca6bc06110cd12bb6366' // sender address 
+const account2 = '0xc40b6909eb7085590e1c26cb3becc25368e249e9' // reciever address 
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider('https://public-en-baobab.klaytn.net')
+  const wallet = new KlaytnWallet(privateKey1, provider);
+
+  // https://docs.klaytn.foundation/content/klaytn/design/transactions/basic#txtypevaluetransfer
+  let tx = {
+      type: 8,          // TxTypeValueTransfer must be 0x08
+      gasPrice: 25e9,
+      gasLimit: 30000,  // Must be fixed value, because it calls deprecated old eth_estimateGas API of Klaytn node
+      to: account2,
+      value: 1e12,
+      from: account1,
+    }; 
+  
+  if (1) {
+    // One-shot (recommended)
+    const sentTx = await wallet.sendTransaction(tx);
+    console.log('sentTx', sentTx);
+
+    const rc = await sentTx.wait();
+    console.log('receipt', rc);
+  } else {
+    // Step-by-step
+    popTx = await wallet.populateTransaction(tx);
+    console.log('tx', popTx);
+
+    const rawTx = await wallet.signTransaction(popTx);
+    console.log('rawTx', rawTx);
+
+    const txhash = await provider.send("klay_sendRawTransaction", [rawTx]);
+    console.log('txhash', txhash);
+
+    const rc = await provider.waitForTransaction(txhash);
+    console.log('receipt', rc);
+  }
+}
+
+main();
