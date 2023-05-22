a4c1e67591d00212c8a54d75a0c249fe84663cdd
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-05-22 13:15:07 +0900

    Delete adjusting Gas logic after estimating

--- src/ethers/signer.ts
+++ src/ethers/signer.ts
@@ -89,9 +89,13 @@ export class KlaytnWallet extends Wallet {
         let ttx = encodeTxForRPC( estimateGasAllowedKeys, tx );
 
         const result = await this.provider.send("klay_estimateGas", [ttx]);
-        // multiply 1.5 for ensuring that the estimated gas is sufficient
-        // refer https://github.com/MetaMask/metamask-extension/blob/9d38e537fca4a61643743f6bf3409f20189eb8bb/ui/ducks/send/helpers.js#L115
-        tx.gasLimit = result*1.5;  
+        // For the problem that estimateGas does not exactly match, 
+        // the code for adding some Gas must be processed in the wallet or Dapp.
+        // e.g. 
+        //   In ethers, no special logic to modify Gas
+        //   In Metamask, multiply 1.5 to Gas for ensuring that the estimated gas is sufficient
+        //   https://github.com/MetaMask/metamask-extension/blob/9d38e537fca4a61643743f6bf3409f20189eb8bb/ui/ducks/send/helpers.js#L115
+        // tx.gasLimit = result*1.5;  
         console.log('gasLimit', result)
       } else {
         throw new Error(`Klaytn transaction can only be populated from a Klaytn JSON-RPC server`);
