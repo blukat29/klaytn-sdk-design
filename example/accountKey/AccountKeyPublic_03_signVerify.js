824f7920c0589f3d08902f062655a053f2cb0a63
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-05-31 21:15:19 +0900

    Update AccountKeyPublic_03_signVerify.js

--- example/accountKey/AccountKeyPublic_03_signVerify.js
+++ example/accountKey/AccountKeyPublic_03_signVerify.js
@@ -9,16 +9,14 @@ const fs = require('fs');
 // https://docs.klaytn.foundation/content/klaytn/design/accounts#accountkeypublic
 // 
 
+const provider = new ethers.providers.JsonRpcProvider('https://public-en-baobab.klaytn.net');
+
 // the same address of sender in AccountKeyPublic_01_accountUpdate.js 
 const sender = '0x1173d5dc7b5e1e07d857d74e962b6ed7d4234a92';
 
-// newly updated private key of sender
-const updated_priv = fs.readFileSync('./example/privateKey', 'utf8');
-
-
-const provider = new ethers.providers.JsonRpcProvider('https://public-en-baobab.klaytn.net');
-
 async function doSignMessage( message ) {
+  // newly updated private key of sender
+  const updated_priv = fs.readFileSync('./example/privateKey', 'utf8');
   const wallet = new KlaytnWallet( sender, updated_priv, provider );
 
   const signature = await wallet.signMessage(message);
